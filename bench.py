0903b28609ea300c5583f4ea4fb95cd9707f5b6e
Darius Foo <darius.foo.tw@gmail.com>
2021-09-09 23:37:59 +0800

    Collect benchmark results and run multiple times

--- bench.py
+++ bench.py
@@ -5,6 +5,7 @@ import pathlib
 import os
 import time
 import shutil
+import re
 
 def mkdirp(path):
   pathlib.Path(path).mkdir(parents=True, exist_ok=True)
@@ -28,15 +29,16 @@ def start_client(f):
     env={'CLIENT': '1'},
     cwd=os.getcwd())
 
-if __name__ == "__main__":
-  replica_count = 2
-  
+def clean():
   shutil.rmtree('out', ignore_errors=True)
   shutil.rmtree('data', ignore_errors=True)
   shutil.rmtree('logs', ignore_errors=True)
   mkdirp('out')
+
+def build():
   subprocess.check_call(['go', 'build', 'main.go'])
 
+def run_experiment(replica_count):
   replica_files = []
   replica_processes = []
   with open('out/master.log', 'w') as master_f, open('out/client.log', 'w') as client_f:
@@ -59,4 +61,46 @@ if __name__ == "__main__":
 
     master_process.terminate()
     master_process.wait()
-  print('done')
+
+def collect_data(replica_count):
+  with open('out/client.log', 'r') as f:
+    for m in re.finditer(r'Total time taken: (\d+)', f.read()):
+      client_time = int(m.group(1))
+      print('client time', client_time)
+
+  monitor_time = 0
+  with open('out/master.log', 'r') as f:
+    for m in re.finditer(r'Monitor time taken: (\d+)', f.read()):
+      t = int(m.group(1))
+      monitor_time += t
+      print('master time', t)
+
+  for i in range(replica_count):
+    with open(f'out/replica{i}.log', 'r') as f:
+      for m in re.finditer(r'Monitor time taken: (\d+)', f.read()):
+        t = int(m.group(1))
+        monitor_time += t
+        print('replica', i, 'time', t)
+
+  print('monitor time', monitor_time)
+  return monitor_time, client_time
+
+if __name__ == "__main__":
+  runs = 5
+  replica_count = 2
+  build()
+  monitor_time = 0
+  client_time = 0
+  for i in range(runs):
+    print(f'--- run {i} ---')
+    clean()
+    run_experiment(replica_count)
+    m, c = collect_data(replica_count)
+    monitor_time += m
+    client_time += c
+
+  monitor_time /= runs
+  client_time /= runs
+  print(f'---')
+  overhead = monitor_time / client_time * 100
+  print(f'overhead: {overhead:.3f}%')
